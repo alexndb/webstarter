55f5bd25216e6db3b692b58458ff3d9722d9ff7e
Aleksandr Seryakov <noz2008@mail.ru>
2021-02-06 00:21:37 +0300

    перерабатывает tooltip под новую версию tippy

--- src/blocks/tooltip/tooltip.js
+++ src/blocks/tooltip/tooltip.js
@@ -6,10 +6,26 @@ export default (() => {
   if (tooltips) {
     for (const tooltip of tooltips) {
       tippy(tooltip, {
-        maxWidth: '250px',
-        arrow: true,
-        placement: 'bottom',
+        appendTo: 'parent',
+        // trigger: 'click',
+        // hideOnClick: false,
+        // animation: 'perspective',
+        // maxWidth: '250px',
+        arrow: false,
+        placement: 'bottom-start',
+        allowHTML: true,
         content: tooltip.children[1].innerHTML
+        // interactive: true,
+        // onShow(instance) {
+        //   instance.popper.querySelector('.tooltip-close').addEventListener('click', () => {
+        //     instance.hide()
+        //   })
+        // },
+        // onHide(instance) {
+        //   instance.popper.querySelector('.tooltip-close').removeEventListener('click', () => {
+        //     instance.hide()
+        //   })
+        // }
       })
     }
   }
